29faaca5c6c2af00d052b8fd9b14f475e51193fb
feranmi <feranmij10@gmail.com>
2026-08-08 18:20:06 +0100

    Fix meals API for Vercel

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,3 +1,45 @@
+// import MealItem from "./MealItem.jsx";
+// import useHttp from "../hooks/useHttp.js";
+// import Error from "./Error.jsx";
+
+// const requestConfig = {};
+
+// export default function Meals() {
+//   const {
+//     data: loadedMeals,
+//     isLoading,
+//     error,
+//   } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+//   if (isLoading) {
+//     return <p className="center">Fetching meals...</p>;
+//   }
+
+//   if (error) {
+//     return (
+//       <Error
+//         title="Failed to fetch meals"
+//         message={error}
+//       />
+//     );
+//   }
+
+//   // if (!data) {
+//   //   return <p>No meals found.</p>
+//   // }
+
+//   return (
+//     <ul id="meals">
+//       {loadedMeals.map((meal) => (
+//         <MealItem
+//           key={meal.id}
+//           meal={meal}
+//         />
+//       ))}
+//     </ul>
+//   );
+// }
+
 import MealItem from "./MealItem.jsx";
 import useHttp from "../hooks/useHttp.js";
 import Error from "./Error.jsx";
@@ -9,24 +51,20 @@ export default function Meals() {
     data: loadedMeals,
     isLoading,
     error,
-  } = useHttp("http://localhost:3000/meals", requestConfig, []);
+  } = useHttp("/api/meals", requestConfig, []);
 
   if (isLoading) {
-    return <p className="center">Fetching meals...</p>;
+    return <p>Fetching meals...</p>;
   }
 
   if (error) {
     return (
       <Error
         title="Failed to fetch meals"
-        message={error}
+        message={error.message}
       />
     );
   }
-
-  // if (!data) {
-  //   return <p>No meals found.</p>
-  // }
 
   return (
     <ul id="meals">
